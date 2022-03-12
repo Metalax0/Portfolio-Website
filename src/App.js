3518e5e4e2755cef4aa252e90d10342fa593b647
Metalax <sampannapokharel@gmail.com>
2022-03-12 23:55:03 +0545

    added contents to CV Page, made changes to navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import HomePage from "./HomePage";
 import CVPage from "./CVPage";
 import ProjectPage from "./ProjectPage";
@@ -10,37 +10,87 @@ import ErrorPage from "./ErrorPage";
 function App() {
   var [navActive, setnavActive] = useState(["activeLink", "", "", "", ""]);
 
+  function handleOnClick(arrIndex) {
+    if (arrIndex == 0) setnavActive(["activeLink", "", "", "", ""]);
+    else if (arrIndex == 1) setnavActive(["", "activeLink", "", "", ""]);
+    else if (arrIndex == 2) setnavActive(["", "", "activeLink", "", ""]);
+    else if (arrIndex == 3) setnavActive(["", "", "", "activeLink", ""]);
+    else if (arrIndex == 4) setnavActive(["", "", "", "", "activeLink"]);
+  }
+
+  useEffect(() => {
+    let currentURL = window.location.pathname;
+    if (currentURL == "/cv") {
+      setnavActive(["", "activeLink", "", "", ""]);
+    } else if (currentURL == "/project") {
+      setnavActive(["", "", "activeLink", "", ""]);
+    } else if (currentURL == "/blog") {
+      setnavActive(["", "", "", "activeLink", ""]);
+    } else if (currentURL == "/contact") {
+      setnavActive(["", "", "", "", "activeLink"]);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <nav>
         <div className="nav-logotext">SAMPANNA</div>
         <div className="nav-menuitem">
-          <Link className={navActive[0]} to="/">
+          <Link
+            className={navActive[0]}
+            onClick={() => handleOnClick(0)}
+            to="/"
+          >
             Home
           </Link>
-          <Link className={navActive[1]} to="/cv">
+          <Link
+            className={navActive[1]}
+            onClick={() => handleOnClick(1)}
+            to="/cv"
+          >
             CV
           </Link>
-          <Link className={navActive[2]} to="/project">
+          <Link
+            className={navActive[2]}
+            onClick={() => handleOnClick(2)}
+            to="/project"
+          >
             PROJECT
           </Link>
-          <Link className={navActive[3]} to="/blog">
+          <Link
+            className={navActive[3]}
+            onClick={() => handleOnClick(3)}
+            to="/blog"
+          >
             BLOG
           </Link>
-          <Link className={navActive[4]} to="/contact">
+          <Link
+            className={navActive[4]}
+            onClick={() => handleOnClick(4)}
+            to="/contact"
+          >
             CONTACT
           </Link>
         </div>
       </nav>
       <Routes>
-        <Route path="/" element={<HomePage changeState={setnavActive}/>} />
-        <Route path="/cv" element={<CVPage changeState={setnavActive}/>} />
-        <Route path="/project" element={<ProjectPage changeState={setnavActive}/>} />
-        <Route path="/blog" element={<BlogPage changeState={setnavActive}/>} />
-        <Route path="/contact" element={<ContactPage changeState={setnavActive}/>} />
+        <Route path="/" element={<HomePage setnavActive={setnavActive} />} />
+        <Route path="/cv" element={<CVPage setnavActive={setnavActive} />} />
+        <Route
+          path="/project"
+          element={<ProjectPage setnavActive={setnavActive} />}
+        />
+        <Route
+          path="/blog"
+          element={<BlogPage setnavActive={setnavActive} />}
+        />
+        <Route
+          path="/contact"
+          element={<ContactPage setnavActive={setnavActive} />}
+        />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
-      <footer>*This is a placeholder for footer*</footer>
+      {/* <footer>*This is a placeholder for footer*</footer> */}
     </BrowserRouter>
   );
 }
--- src/CVPage/index.js
+++ src/CVPage/index.js
@@ -3,7 +3,166 @@ import "./index.css";
 const CVPage = () => {
   return (
     <div className="CVPage">
-      <h1> This is CV Page</h1>
+      {/* Left Column on CV page */}
+      <div className="CVContainerLeft">
+        {/* Image Section of CV Page */}
+        <div className="CVImage">
+          <img src="img_Portfolio3.jpg" />
+          <label className="CVTitle">SAMPANNA POKHAREL</label>
+          <label className="CVSubTitle">STUDENT</label>
+        </div>
+        {/* Profile Section of CV Page */}
+        <div className="CVProfile">
+          <label className="CVTitle">PROFILE</label>
+          <p className="CVDescription">
+            <br />I am Sampanna Pokharel and am currently a student. I am
+            pursuing my bachelors degree in computing at Islington College,
+            Kathmandu, Nepal. In general, my field of interest is in front-end
+            web development and programming.
+          </p>
+        </div>
+        {/* Contact Section of CV Page */}
+        <ul className="CVContact">
+          <br />
+          <label className="CVTitle">CONTACT</label>
+          <br />
+          <li>
+            <img className="imgIcon" src="icon_address.png" />
+            <a
+              target="_blank"
+              href="https://www.google.com/maps/place/Kathmandu+44600/data=!4m2!3m1!1s0x39eb198a307baabf:0xb5137c1bf18db1ea?sa=X&ved=2ahUKEwj1-rHukcH2AhW8UvUHHRVZDv8Q8gF6BAgREAE"
+            >
+              Gaushala, Kathmandu, Nepal
+            </a>
+          </li>
+          <li>
+            <img className="imgIcon" src="icon_phone.png" />
+            +9779869277513
+          </li>
+          <li>
+            <img className="imgIcon" src="icon_email.png" />{" "}
+            <a
+              target="_blank"
+              href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGLrRTJqHlJJwWfKnHvTwHbrzFcHPpZxsZhZLCjMcrRFqNSNMfNpSZJcxdRHKqsnJXPRrhS"
+            >
+              sampannapokharel@gmail.com
+            </a>
+          </li>
+
+          <li>
+            <img className="imgIcon" src="icon_website.png" />
+            <a target="_blank" href="http://sampannapokharel.com.np/">
+              sampannapokharel.com.np
+            </a>
+          </li>
+          <li>
+            <img className="imgIcon" src="icon_github.png" />
+            <a target="_blank" href="https://github.com/Metalax0">
+              github.com/Metalax0
+            </a>
+          </li>
+          <li>
+            <img className="imgIcon" src="icon_facebook.png" />
+            <a
+              target="_blank"
+              href="https://www.facebook.com/sampanna.pokharel.0/"
+            >
+              facebook.com/sampanna.pokharel.0
+            </a>
+          </li>
+        </ul>
+        <a href="Sampanna_Pokharel_CV.pdf" download>
+          <button>Download CV</button>
+        </a>
+      </div>
+
+      <div className="CVContainerRight">
+        {/* Education Section of CV Page */}
+        <div className="CVEducation">
+          <label className="CVTitle">EDUCATION</label>
+          <p className="CVDescription">
+            <br />
+            <b>A-Levels, 2018 :</b> Completed my A-Levels from Trinity
+            International College, Kathmandu, Nepal with major in Computer
+            Science.
+            <br />
+            <br />
+            <b>Bachelors, current :</b> Currently studying bachelors in
+            computing at Islington College, Kathmandu, Nepal. Expected to
+            graduate in 2023
+          </p>
+        </div>
+        {/* Projects Section of CV Page */}
+        <div className="CVProjects">
+          <label className="CVTitle">PROJECTS</label>
+          <p className="CVDescription">
+            <br />
+            <b>Clover.Three :</b> Clover.Three is a front end web project
+            primarily developed in JavaScript (two version available - react and
+            vanila js). It was a group project consisting of three members.
+            <br />
+            <br />
+            <b>CV/Portfolio :</b> CV/Portfolio website is a portfolio website
+            build using HTML, CSS and Javascript. It was developed as a part of
+            college assignment.
+            <br />
+            <br />
+            <b>Space Rush :</b> Space Rush is a browser game primarily developed
+            in vanilla javascript.
+          </p>
+        </div>
+        {/* Hard Skills Section of CV Page */}
+        <div className="CVHardSkills">
+          <label className="CVTitle">HARD SKILLS</label>
+          <ul className="CVHardSkillsList">
+            <div className="HardSkillsLeft">
+              <li>HTML</li>
+              <li>React</li>
+            </div>
+            <div className="HardSkillsMiddle">
+              <li>CSS</li>
+              <li>SQL</li>
+            </div>
+            <div className="HardSkillsRight">
+              <li>JavaScript</li>
+              <li>Python</li>
+            </div>
+          </ul>
+        </div>
+        {/* Soft Skills Section of CV Page */}
+        <div className="CVSoftSkills">
+          <label className="CVTitle">SOFT SKILLS</label>
+          <ul className="CVSoftSkillsList">
+            <li>
+              <b>Leadership -</b> Student Academic Representative in college,
+              Supervised group project in friend group, Took leadership 1.0
+              class at Nobel Internship (current)
+            </li>
+            <br />
+            <li>
+              <b>Communication, Teamwork -</b> Worked in a team to build web
+              projects, worked with an internation team during internship at
+              Nobel (current)
+            </li>
+          </ul>
+        </div>
+        {/* Technical Skills Section of CV Page */}
+        <div className="CVTechSkills">
+          <label className="CVTitle">TECHNICAL SKILLS</label>
+          <ul className="CVTechSkillsList">
+            <li>
+              <b>Git/GitHub -</b> Familiar with basics of Git and GitHub
+            </li>
+            <li>
+              <b>Netbeans/Sublime/VS Code -</b>Fair experience working with
+              these IDEs
+            </li>
+            <li>
+              <b>Adobe Illustrator -</b> Familiar with basics of Illustrator
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
